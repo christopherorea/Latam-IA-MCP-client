72d61ed5399da7f02d0f8ee3a0548710ecdf896e
christopherorea <c28_09_98@hotmail.com>
2025-06-27 04:39:02 -0600

    Add icons and screenshots for PWA support; update package dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,52 +1,61 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import commonjs from 'vite-plugin-commonjs';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(() => {
-    return {
-      base: './', // Ajustado para Chrome Extension
-      define: {
-        'global': 'window',
-      },
-      plugins: [
-        commonjs(),
-        nodePolyfills({
-          protocolImports: true
-        }),
-        tailwindcss(),
+  return {
+    base: './', // Ajustado para Chrome Extension
+    define: {
+      'global': 'window',
+    },
+    plugins: [
+      commonjs(),
+      nodePolyfills({
+        protocolImports: true
+      }),
+      tailwindcss(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'icons',
+            dest: 'assets'
+          }
+        ]
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+        'node:async_hooks': path.resolve(__dirname, 'stubs/async_hooks.js'),
+      }
+    },
+    optimizeDeps: {
+      exclude: [
+        '@langchain/core'
       ],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-          'node:async_hooks': path.resolve(__dirname, 'stubs/async_hooks.js'),
-        }
+      include: [
+        'camelcase',
+        'decamelize',
+        'p-queue',
+        'p-retry',
+        'semver',
+        'ansi-styles',
+        'base64-js'
+      ],
+      esbuildOptions: {
+        plugins: [
+          NodeGlobalsPolyfillPlugin({
+            process: true,
+            buffer: true
+          }),
+          NodeModulesPolyfillPlugin(),
+        ]
       },
-      optimizeDeps: {
-        exclude: [
-          '@langchain/core'
-        ],
-        include: [
-          'camelcase',
-          'decamelize',
-          'p-queue',
-          'p-retry',
-          'semver',
-          'ansi-styles',
-          'base64-js'
-        ],
-        esbuildOptions: {
-          plugins: [
-            NodeGlobalsPolyfillPlugin({
-              process: true,
-              buffer: true
-            }),
-            NodeModulesPolyfillPlugin(),
-          ]
-        },
-      },
-    };
+    },
+  };
 });
